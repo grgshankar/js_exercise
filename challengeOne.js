cb7631087ffe71bf1329c71ddff4e8f0b2f691a4
Crisp Digital <cdadmin@macs-MacBook-Pro.local>
2023-08-08 00:38:55 +0545

    doing challenge one

---
+++ challengeOne.js
@@ -0,0 +1,15 @@
+// Write a JavaScript program to compute the sum of the two given integers. If the two values are the same, then return triple their sum.
+
+// hard way
+// const val1 = 10;
+// const val2 = 10;
+// const sum = val1 + val2;
+// console.log(sum);
+
+//  by using prompt
+const val1 = Number(prompt("Enter first Number"));
+console.log(val1);
+const val2 = Number(prompt("Enter second Number"));
+console.log(val2);
+const sum = val1 + val2;
+alert("The total number is = " + sum);
---
+++ challengeZero.js
@@ -0,0 +1,6 @@
+// Write a JavaScript program that prints 'Hello World'.
+// console.log("hello world!!!");
+
+// Write a JavaScript program to set the value of PI.
+const pi = Math.PI;
+console.log(pi);
